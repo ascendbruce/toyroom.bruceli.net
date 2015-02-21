fd6e3cc04da66507b8de733936dd21bdb713b38c
Bruce Li <ascendbruce@gmail.com>
2015-02-21 20:35:11 +0800

    Refactor RenderPostImageTag by getting config via context
    Learned from http://stackoverflow.com/questions/7666236/how-to-pass-a-variable-into-a-custom-tag-in-liquid

--- _plugins/jekyll-post-image-tag.rb
+++ _plugins/jekyll-post-image-tag.rb
@@ -4,13 +4,12 @@ module Jekyll
     def initialize(tag_name, text, tokens)
       super
       @text = text.strip
-      config = Jekyll.configuration({})
-      @url = config["url"]
-      @baseurl = config["baseurl"]
     end
 
     def render(context)
-      %Q(<a href="#{@url}#{@baseurl}/images/posts/#{@text}"><img src="#{@url}#{@baseurl}/images/posts/#{@text}" alt=""></a>)
+      url     = context.environments.first["site"]["url"]
+      baseurl = context.environments.first["site"]["baseurl"]
+      %Q(<a href="#{url}#{baseurl}/images/posts/#{@text}"><img src="#{url}#{baseurl}/images/posts/#{@text}" alt=""></a>)
     end
   end
 end
