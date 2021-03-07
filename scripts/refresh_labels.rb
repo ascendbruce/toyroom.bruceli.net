371bed5380592d0f252098f8f4572eae5e40a060
Bruce Li <ascendbruce@gmail.com>
2021-03-07 16:04:11 +0800

    Add blogger label (tag) page

---
+++ scripts/refresh_labels.rb
@@ -0,0 +1,68 @@
+# USAGE
+#
+# 1. copy the latest label names from the Archive page, format and paste into the tags array below
+# 2. Run `ruby scripts/refresh_labels.rb`
+
+tags = %w[
+  學習成功部落格
+  小技巧
+  聽演講心得
+  雜項
+  讀書心得
+  公告
+  作品
+  心情日記
+  成功的觀念
+  翻譯國外好文
+  商管、創業
+  個人發展
+  教育
+  全人-感情
+  領導
+  全人-心智
+  全人-心靈
+  輔助工具
+  全人-健康
+  夢境
+  遊記
+  嘉言精選
+  食記
+  我覺得不錯的終極用品
+  怎麼唸
+  Mac
+  有趣好笑
+  Ubuntu
+  linux
+  ror
+  音樂
+  了解新知與探究常識
+  git
+  rails
+  ruby
+  經驗學習
+  mysql
+  iphone
+  mac
+  Sublime
+  career
+  pow
+  Techbang
+  productivity
+  雜想
+]
+
+Dir.foreach("_search_label") do |f|
+  fn = File.join("_search_label", f)
+  File.delete(fn) if f != '.' && f != '..'
+end
+
+tags.each do |tag|
+  File.open("_search_label/#{tag}.md", "wb") do |file|
+    file << <<~TEXT
+      ---
+      layout: search_label
+      tag-name: #{tag}
+      ---
+    TEXT
+  end
+end
